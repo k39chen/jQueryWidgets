181a28e9ca9dec7640622bdd0103d50013332269
Kevin Chen <k39chen@gmail.com>
2013-11-09 11:07:26 -0800

    Added generic content to the base plugin

---
+++ base/demo/js/demo.js
@@ -0,0 +1,12 @@
+/**
+ * demo.js
+ *
+ * @author Kevin Chen <k39chen@gmail.com>
+ * @copyright 20[date], Kevin Chen, All rights reserved.
+ */
+$(document).ready(function(){
+
+	// initialize the widget
+	$("#test")["[name]"]();
+
+});
---
+++ base/src/jquery.[name].js
@@ -0,0 +1,34 @@
+/**
+ * jquery.[name].js
+ *
+ * @author Kevin Chen <k39chen@gmail.com>
+ * @copyright 20[date], Kevin Chen, All rights reserviced.
+ */
+$.widget("jQuery.[name]", {
+	options: {
+		// ...
+	},
+	_create: function(){
+		var self = this,
+			el = self.element;
+		
+		el.addClass('[class-name]');
+		
+		// ...
+	},
+	_setOptions: function(){
+		this._superApply(arguments);
+		this._create();
+	},
+	_setOption: function(key, value) {
+		this._super(key, value);
+	},
+	_destroy: function() {
+		var self = this,
+			el = self.element;
+			
+		el.removeClass('[class-name]');
+		
+		// ...
+	}
+});
