c218863a8036c1337f646f94e3ab7f38f7f74933
Kevin Chen <kchen@tagged.com>
2013-11-11 22:58:48 -0800

    More work on banner

--- base/docs/js/main.js
+++ base/docs/js/main.js
@@ -8,15 +8,22 @@
 $(document).ready(function(){
 
 
-	$(".nav-item a").easyHover({
-		start: {color: "#aaa"},
+	$(".nav-link").easyHover({
+		start: {color: "#bbb"},
 		end: {color: "#fff"}
 	}).click(function(){
-		var anchor = $( $(this).attr("href") );
-		$("html,body").stop().animate({scrollTop:anchor.offset().top},300);
+		var anchor = $( $(this).attr("dest") );
+		$("html,body").stop().animate({scrollTop:anchor.offset().top},700);
 	});
 
 	$("#download-btn").easyButton();
+
+	$(".browser").each(function(){
+		$(this).easyHover({
+			start: {opacity: ($(this).hasClass("supported")) ? 0.8 : 0.1},
+			end: {opacity: ($(this).hasClass("supported")) ? 1.0 : 0.2}
+		});
+	});
 
 	$("#demo-1").smartImage({ width: 300, height: 250, hCenter: -32 });
 	$("#demo-2").smartImage({ width: 400, height: 400, hLeft: 0 });
